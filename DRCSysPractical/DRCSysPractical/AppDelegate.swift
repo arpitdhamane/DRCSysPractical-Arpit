857abfbefedc9b1932e4c46beecca323db9eedcd
arpitdhamane <arpitdhamane@gmail.com>
2020-12-09 22:56:23 +0530

    Initial Commit and Removing scene delegate with it.

---
+++ DRCSysPractical/DRCSysPractical/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DRCSysPractical
+//
+//  Created by Arpit Dhamane on 09/12/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
